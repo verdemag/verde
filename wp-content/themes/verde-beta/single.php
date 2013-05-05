b07710aa3bc7e759ac1c1d611a5c731abc5b0922
Sauyon Lee <git@sauyon.com>
2013-05-04 19:55:07 -0700

    simple share/tweet button

--- wp-content/themes/verde-beta/single.php
+++ wp-content/themes/verde-beta/single.php
@@ -1,3 +1,2 @@
-<?php get_header(); ?>
-
-<?php get_footer(); ?>
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://hi.com" data-text="Check this out" data-via="verdemagazine" data-dnt="true">Tweet</a>
+<div class="fb-send" data-href="http://hi.com"></div>
