5a939ddeda78d3edcd6f27c92ebb802d78d78960
Sauyon Lee <git@sauyon.com>
2013-05-09 19:19:45 -0700

    added category -_-

---
+++ wp-content/themes/verde-beta/templates/category.php
@@ -0,0 +1,29 @@
+<?php
+class category {
+  private $posts;
+
+  function __construct($c) {
+    $this->posts = get_posts(array('numberposts' => 4,
+                                   'category' => $c));
+  }
+
+  public function getPageContents() {
+    $ret = ('<div class="grid_8 alpha">');
+    foreach ($this->posts as $post) {
+      $tagless = strip_tags($post->post_content);
+      $content = implode(' ', array_slice(explode(' ', $tagless), 0, 50));
+      $ret .= ('<div class="grid_4 content">'
+              . '<img alt="" src="http://placehold.it/300x300">'
+              . '<h1>' . $post->post_title . '</h1>'
+              . '<p>' . $content . '</p>'
+              . '</div>');
+    }
+    $ret .= ('</div>'
+            . '<div class="grid_4 omega">'
+            . file_get_contents(get_template_directory_uri() . '/sidebar.php')
+            . '</div>');
+
+    return $ret;
+  }
+}
+?>
