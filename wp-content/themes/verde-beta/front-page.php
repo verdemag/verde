1b09d65435f385a4ce6d22a93db842c1b37d8756
Sauyon Lee <git@sauyon.com>
2013-05-09 19:31:35 -0700

    quick test commit

--- wp-content/themes/verde-beta/front-page.php
+++ wp-content/themes/verde-beta/front-page.php
@@ -6,8 +6,6 @@
 $categories = get_categories(array('sort_column' => 'menu_order',
                                    'hide_empty' => 0,
                                    'number' => 6));
-$pages = get_pages(array('sort_column' => 'menu_order',
-                         'number' => 8));
 foreach ($categories as $category) : ?>
 <div class="container_12 page" id="<?php echo $category->slug ?>">
   <?php echo getPage($category) ?>
