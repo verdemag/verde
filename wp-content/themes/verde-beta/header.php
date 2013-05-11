85a980332d95f8e1bfa9e614b5c5b3e9d64e3ade
Sauyon Lee <git@sauyon.com>
2013-05-10 18:18:15 -0700

    made the logo link work and made home button auto selected on page load

--- wp-content/themes/verde-beta/header.php
+++ wp-content/themes/verde-beta/header.php
@@ -19,21 +19,21 @@
 
     <?php wp_head(); ?>
   </head>
-  
+
   <body>
     <div id="fb-root"></div>
     <div class="container_16">
       <div id="logo">
-	<div class="title"><?php bloginfo('name'); ?></div>
-	<div class="sub"><?php bloginfo('description'); ?></div>
+	      <div class="title"><?php bloginfo('name'); ?></div>
+	      <div class="sub"><?php bloginfo('description'); ?></div>
       </div>
       <div class="grid_16 navBar">
-        <div class="grid_2 navLink" id="homelink">Home</div>
+        <div class="grid_2 navLink selected" id="homelink">Home</div>
         <?php $categories = get_categories(array('sort_column' => 'menu_order',
                                                  'hide_empty' => 0,
                                                  'number' => 6)); ?>
         <?php foreach ($categories as $category) : ?>
-	<div class="grid_2 navLink"
+	      <div class="grid_2 navLink"
              id="<?php echo $category->slug ?>link"><?php echo $category->name ?></div>
         <?php endforeach; ?>
         <div class="grid_2 navLink" id="aboutlink">About</div>
--- wp-content/themes/verde-beta/js/main.js
+++ wp-content/themes/verde-beta/js/main.js
@@ -1,83 +1,83 @@
 jQuery(document).ready(function() {
-		window.footer = jQuery('#footer');
-		footer.collapseHeight = footer.find('.row').first().height();
-		footer.css('height','auto');
-		footer.totalHeight = footer.height();
-		footer.css('height', footer.collapseHeight);
-		
-		window.pages = jQuery('.page');
-		window.selected = pages.first();
-		window.mask = jQuery('#mask');
-		window.wrapper = jQuery('#wrapper');
-		window.zoomed= true;
+	window.footer = jQuery('#footer');
+	footer.collapseHeight = footer.find('.row').first().height();
+	footer.css('height','auto');
+	footer.totalHeight = footer.height();
+	footer.css('height', footer.collapseHeight);
 
-    jQuery('#logo').click(function(event) {
-				switchToItem('#page0');
-    });
-    
-    jQuery('.newsButton').click(function(event) {
-				jQuery(event.target).parent().parent().animate({maxHeight: 300}, 1000);
-				jQuery(event.target).parent().animate({height: 0}, 300);
-				jQuery(event.target).fadeOut(700);
-    });
-    
-    jQuery('.navLink').click(function(event) {
-				var id = event.target.id;
-				var targetID = '#' + id.substring(0, id.length - 4);
-				console.debug(targetID);
+	window.pages = jQuery('.page');
+	window.selected = pages.first();
+	window.mask = jQuery('#mask');
+	window.wrapper = jQuery('#wrapper');
+	window.zoomed= true;
 
-				switchToItem(targetID);
-    });
+	jQuery('#logo').click(function(event) {
+		switchToItem('#home');
+	});
 
-    jQuery(window).scroll(function(event) {
-				if(jQuery(window).scrollTop() - jQuery("body").offset().top >= 130) {
-						jQuery(".navBar").addClass("fixed");
-				} else {
-						jQuery(".navBar").removeClass("fixed");
-				}
-    });
+	jQuery('.newsButton').click(function(event) {
+		jQuery(event.target).parent().parent().animate({maxHeight: 300}, 1000);
+		jQuery(event.target).parent().animate({height: 0}, 300);
+		jQuery(event.target).fadeOut(700);
+	});
 
-		footer.hover(function(){
-				footer.stop().animate({height:footer.totalHeight}, 500);
-		},function(){
-				footer.stop().animate({height:footer.collapseHeight}, 500);
-		});
+	jQuery('.navLink').click(function(event) {
+		var id = event.target.id;
+		var targetID = '#' + id.substring(0, id.length - 4);
+		console.debug(targetID);
 
-		jQuery('#zoombutton').click(function(event) {
-				if(zoomed) {
-						wrapper.stop().animate({zoom:1/6}, 500);
-						window.mask.stop().animate({height:window.wrapper.height()}, 500);
-						zoomed = false;
-				} else {
-						wrapper.stop().animate({zoom:1}, 500);
-						resizeMask();
-						zoomed = true;
-				}
-		});
+		switchToItem(targetID);
+	});
 
-		//jQuery('#logo .title').lettering();
-		resizeMask();
-		genLinks();
+	jQuery(window).scroll(function(event) {
+		if(jQuery(window).scrollTop() - jQuery("body").offset().top >= 130) {
+			jQuery(".navBar").addClass("fixed");
+		} else {
+			jQuery(".navBar").removeClass("fixed");
+		}
+	});
+
+	footer.hover(function(){
+		footer.stop().animate({height:footer.totalHeight}, 500);
+	},function(){
+		footer.stop().animate({height:footer.collapseHeight}, 500);
+	});
+
+	jQuery('#zoombutton').click(function(event) {
+		if(zoomed) {
+			wrapper.stop().animate({zoom:1/6}, 500);
+			window.mask.stop().animate({height:window.wrapper.height()}, 500);
+			zoomed = false;
+		} else {
+			wrapper.stop().animate({zoom:1}, 500);
+			resizeMask();
+			zoomed = true;
+		}
+	});
+
+	//jQuery('#logo .title').lettering();
+	resizeMask();
+	genLinks();
 });
 
 function highlightItem(item) {
-		jQuery('.navLink.selected').removeClass('selected');
-		item.addClass('selected');
+	jQuery('.navLink.selected').removeClass('selected');
+	item.addClass('selected');
 }
 
 function switchToItem(ID) {
-		var linkID = ID + 'link';
-		window.selected = jQuery(ID);
+	var linkID = ID + 'link';
+	window.selected = jQuery(ID);
 
-		highlightItem(jQuery(linkID));
-		window.wrapper.animate({top:-selected.position().top, left:-selected.position().left}, 500);
-		resizeMask();
+	highlightItem(jQuery(linkID));
+	window.wrapper.animate({top:-selected.position().top, left:-selected.position().left}, 500);
+	resizeMask();
 }
 
 function resizeMask() {
-		window.mask.animate({ height:window.selected.height() + 50 }, 50);
+	window.mask.animate({ height:window.selected.height() + 50 }, 50);
 }
 
 function genLinks() {
-		
+
 }
