d5f96421d837b3db50524d9f2793f700fc74bcdb
Jake Gurle <jake.gurle7@gmail.com>
2013-11-03 17:47:52 -0800

    Post sorting test

--- wp-content/themes/verde-beta/templates/category.php
+++ wp-content/themes/verde-beta/templates/category.php
@@ -5,7 +5,7 @@ class category {
   function __construct($c) {
     $this->posts = get_posts(array('numberposts' => 8,
                                    'category' => $c,
-                                   'orderby'  => 'none'));
+                                   'orderby'  => 'rand'));
   }
 
   public function getPageContents() {
