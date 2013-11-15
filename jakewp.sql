216809a275ed5d55b436a8db4c4bab82b1ed02a2
Sauyon Lee <git@sauyon.com>
2013-11-14 23:20:43 -0800

    deal with the case where the user exists (because mysql is stupid)

--- jakewp.sql
+++ jakewp.sql
@@ -533,5 +533,20 @@ UNLOCK TABLES;
 
 -- Dump completed on 2013-11-13 19:18:18
 
-CREATE USER jake IDENTIFIED BY 'jaketheHacker';
+drop procedure if exists createUser;
+delimiter $$
+create procedure createUser(username varchar(50), pw varchar(50))
+begin
+IF (SELECT EXISTS(SELECT 1 FROM `mysql`.`user` WHERE `user` = username)) = 0 THEN
+    begin
+    set @sql = CONCAT('CREATE USER ', username, '@\'localhost\' IDENTIFIED BY \'', pw, '\'');
+    prepare stmt from @sql;
+    execute stmt;
+    deallocate prepare stmt;
+    end;
+END IF;
+end $$
+delimiter ;
+
+call createUser('jake', 'jaketheHacker');
 GRANT ALL PRIVILEGES ON jakewp.* to jake;
