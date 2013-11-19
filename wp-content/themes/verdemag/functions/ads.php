13582c34f08644dcf44e82d5f2a18498377fa8aa
Sauyon Lee <git@sauyon.com>
2013-11-18 23:13:40 -0800

    forgot to add files -_-;

---
+++ wp-content/themes/verdemag/functions/ads.php
@@ -0,0 +1,64 @@
+<?php
+function create_ad_type() {
+	register_post_type( 'ad',
+		array(
+			'labels' => array(
+				'name' => 'Ads',
+				'singular_name' => 'Ad',
+        'add_new_item' => 'Add New Ad',
+        'edit_item' => 'Edit Ad',
+        'view_item' => 'View Ad',
+        'search_items' => 'Search Ads',
+        'not_found' => 'No ads found',
+        'not_found_in_trash' => 'No ads found in trash'
+			),
+		  'public' => true,
+      'exclude_from_search' => true,
+      'show_in_nav_menus' => false,
+      'show_in_admin_bar' => false,
+      'menu_position' => 5,
+      'supports' => false,
+		  'has_archive' => true
+		)
+	);
+}
+
+function ad_meta_box() {
+  if ( !class_exists( 'RW_Meta_Box' ) )
+  return;
+
+  $meta_box = array(
+    'id' => 'ad_meta_box',
+    'title' => 'Ad Attributes',
+    'pages' => array('ad'),
+    'priority' => 'high',
+    'autosave' => true,
+    'fields' => array(
+      array(
+        'name' => 'Image',
+        'id' => 'img',
+        'type' => 'image_advanced'
+      ),
+      array(
+        'name' => 'URL',
+        'id' => 'url',
+        'type' => 'text',
+        'std' => '#'
+      ),
+    )
+  );
+  new RW_Meta_Box( $meta_box );
+}
+
+function default_custom_fields($post_id) {
+  if ( $_GET['post_type'] == 'ad' ) {
+    add_post_meta($post_id, 'url', '', true);
+  }
+
+  return true;
+}
+
+add_action('wp_insert_post', 'default_custom_fields');
+add_action( 'init', 'create_ad_type' );
+add_action('admin_init', 'ad_meta_box');
+?>
---
+++ wp-content/themes/verdemag/functions/navlinks.php
@@ -0,0 +1,53 @@
+<?php
+
+class verde_walker_nav_menu extends Walker_Nav_Menu {
+	function start_lvl(&$output, $depth=0, $args=Array()) {
+		if($depth > 0) return;
+		$output .= "\n".'<ul class="sub_menu">'."\n";
+	}
+
+	function start_el(&$output, $item, $depth=0, $args=Array(), $id=0) {
+		$ver = isset($_GET['ver']) ? $_GET['ver'] : false;
+
+		if($depth > 1) return;
+		$output .= '<li>';
+		switch ($item->object) {
+			case "category":
+			$cat = get_category($item->object_id);
+			$slug = $cat->slug;
+			global $archive_ID;
+			if($cat->cat_ID == $archive_ID) {
+				$item_output = '<a id="archivelink">';
+			} else if($cat->parent == $archive_ID) {
+				$item_output = "<a href=\"?ver=$slug\">";
+			} else {
+				$link = $ver ? "?ver=$ver&page=$slug" : "?page=$slug";
+				$item_output = "<a class=\"navLink\" href=\"$link\" data-target=\"$slug\">";
+			}
+			break;
+			case "page":
+			$slug = get_post($item->object_id)->post_name;
+			$link = $ver ? "?ver=$ver&page=$slug" : "?page=$slug";
+			$item_output = "<a class=\"navLink\" href=\"$link\" data-target=\"$slug\">";
+			break;
+			case "post":
+			$slug = get_post($item->object_id)->post_name;
+			$link = $ver ? "?ver=$ver&post=$slug" : "?post=$slug";
+			$item_output = "<a class=\"navLink\" href=\"$link\" data-target=\"$slug\">";
+			break;
+			case "custom":
+			if($item->url == site_url()) {
+				$item_output = '<a class="navLink" href="/" data-target="home">';
+				break;
+			}
+			default:
+			error_log("Different object type: ".$item->object."\nWith url: ".$item->url);
+			break;
+		}
+		$item_output .= $item->title;
+		$item_output .= '</a>';
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+}
+
+?>
