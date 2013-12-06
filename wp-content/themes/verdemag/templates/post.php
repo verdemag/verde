c44233d1c199b0eeedcd49370ea24b90fa14cd59
Sauyon Lee <git@sauyon.com>
2013-12-06 05:58:36 -0800

    make social buttons open popup windows instead of new tabs

--- wp-content/themes/verdemag/templates/post.php
+++ wp-content/themes/verdemag/templates/post.php
@@ -20,13 +20,17 @@ class post {
     $ret = '<article>';
     $ret .= "<header><h1>{$this->post['title']}</h1>";
     $ret .= "<h2>{$this->post['subtitle']}</h2>";
+		$ret .= "</header>";
     if($this->post['author'])
       $ret .= "<span class=\"author\">By: {$this->post['author']}</span></header>";
     $ret .= do_shortcode($this->post['content']);
     $ret .= "<a href=\"{$this->url}\">Permalink</a><br /><br />";
-    $ret .= "<a href=\"{$this->fb}\" class=\"social icon-fb\"></a>|";
-    $ret .= "<a href=\"{$this->twit}\" class=\"social icon-twitter\"></a>|";
-    $ret .= "<a href=\"{$this->gplus}\" class=\"social icon-gplus\"></a>";
+    $ret .= "<a href=\"#\" onclick=\"open('{$this->fb}','Share on FB','width=618,height=325')\""
+					."class=\"social icon-fb\"></a>|";
+    $ret .= "<a href=\"#\" onclick=\"open('{$this->twit}','Share on Twitter','width=465,height=275')\""
+					."class=\"social icon-twitter\"></a>|";
+    $ret .= "<a href=\"#\" onclick=\"open('{$this->gplus}','Share on GPlus','width=500,height=375')\""
+					."class=\"social icon-gplus\"></a>";
 
     return $ret;
   }
