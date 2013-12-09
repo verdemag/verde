c5fe1446b0d01173c1fb4b8a181b9eaaa819c9f8
Sauyon Lee <git@sauyon.com>
2013-12-08 18:53:54 -0800

    show excerpt editor by default

--- wp-content/themes/verdemag/functions/post-meta.php
+++ wp-content/themes/verdemag/functions/post-meta.php
@@ -61,6 +61,14 @@ function post_info_meta_box() {
   new RW_Meta_Box( $meta_box );
 }
 
+function verde_hidden_meta_boxes($hidden, $screen) {
+	if ( 'post' == $screen->base || 'page' == $screen->base )
+		$hidden = array('revisionsdiv', 'trackbacksdiv', 'postcustom', 'commentstatusdiv', 'commentsdiv', 'slugdiv', 'authordiv', 'revisionsdiv');
+
+	return $hidden;
+}
+
+add_filter('default_hidden_meta_boxes', 'verde_hidden_meta_boxes', 10, 2);
 add_action('admin_init', 'post_info_meta_box');
 add_action('admin_init', 'cover_meta_box');
 ?>
